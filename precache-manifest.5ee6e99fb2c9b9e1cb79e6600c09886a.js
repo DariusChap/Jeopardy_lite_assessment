47f6f087e0d14f4981b48b6a9e5db5095f1c57fc
Darius <chapd289@robertmorris.edu>
2020-08-31 15:04:18 -0500

    Updates

---
+++ precache-manifest.5ee6e99fb2c9b9e1cb79e6600c09886a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04fefdc6b05c703fab006998b89b205",
+    "url": "/Jeopardy_lite_assessment/index.html"
+  },
+  {
+    "revision": "076e717176ee71ff54c2",
+    "url": "/Jeopardy_lite_assessment/static/css/main.d66234cd.chunk.css"
+  },
+  {
+    "revision": "21fb6e49bf3eca5711aa",
+    "url": "/Jeopardy_lite_assessment/static/js/2.57147659.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jeopardy_lite_assessment/static/js/2.57147659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076e717176ee71ff54c2",
+    "url": "/Jeopardy_lite_assessment/static/js/main.ef27ab83.chunk.js"
+  },
+  {
+    "revision": "c4648ced5a261f59e486",
+    "url": "/Jeopardy_lite_assessment/static/js/runtime-main.a186250a.js"
+  }
+]);
